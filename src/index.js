1a8f9c62abaa48891ce94b7adea3b5974e7fae18
Kael Zhang <i+github@kael.me>
2017-09-01 00:55:31 +0800

    1.0.3: improves README.md, meta info

--- src/index.js
+++ src/index.js
@@ -11,13 +11,18 @@ module.exports = name =>
 
     return replacer
       ? typeof replacer !== STRING
+        // Maybe function
         ? string.replace(r, replacer)
         : string.replace(r, (m, h, d, maxLength) =>
           maxLength = Number(maxLength)
+            // String + sub
             ? replacer.substr(0, maxLength)
+            // Replace string
             : replacer)
       : buffer
+        // Get content hash
         ? string.replace(r, (m, hashType, digestType, maxLength) =>
           digest(buffer, hashType, digestType, maxLength))
+        // Do nothing
         : string
   }
